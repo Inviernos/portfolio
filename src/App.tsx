d506551b44aa1773f86ac5dc4239b6860ba9c68f
Anthony Golubski-Allen <anthony302367@gmail.com>
2023-04-04 20:18:34 -0400

    test jenkins 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload Test commit and win around the w.
+          Edit <code>src/App.tsx</code> and save to reload Test commit and win around the world.
         </p>
         <a
           className="App-link"
