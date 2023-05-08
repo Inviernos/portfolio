09d822fb8b28913ebb9f7856806646a220de2651
Anthony Golubski-Allen <anthony302367@gmail.com>
2023-05-08 01:03:29 -0400

    believe it 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload Test commit and win around the world.This will totally work finally. Believe in medd
+          Edit <code>src/App.tsx</code> and save to reload Test commit and win around the world.This will totally work finally. I beleive in the heart of the cards
         </p>
         <a
           className="App-link"
