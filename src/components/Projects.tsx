56bb6bffaf730b3cffbce882719524fa1b23195c
Anthony Golubski-Allen <anthony302367@gmail.com>
2023-06-29 02:08:37 -0400

    Email is last part

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,9 @@ import { Container, Row, Col , Tab , Nav, Accordion} from "react-bootstrap";
 import client1 from '../assets/img/client1.jpg';
 import client2 from '../assets/img/client2.jpg';
 import client3 from '../assets/img/client3.jpg';
+import project1 from '../assets/img/project1.png';
+import project2 from '../assets/img/project2.png';
+import project3 from '../assets/img/project3.png';
 
 function Projects()
 {
@@ -9,12 +12,12 @@ function Projects()
     const projects = [
         {
             title: "Client 1: World leader in smart buildings, creating safe, healthy and sustainable spaces ",
-            description: "I was assigned to a group that was using an etl tool called talend. I followed a document " +
+            description: "I was assigned to a group that was using an etl tool called talend. The client sent me a laptop to the Infosys office. Once that happened, I followed a document " +
             "to gain access to everything I needed. They assigned me a list of talend jobs that are listed in the " +
             "TAC (Talend Adminstrator Center) in QA to run. That was my day to day. If they failed, I would go check the logs and see what went "+
             "wrong. Most of the time it failed because it was using a hostname instead of an ip address. In the backend, the setup didnt have the hostname connected to the ip " + 
             "address. There would also be jobs that didnt exist. I would go in and create the job in TAC. I would attend morning meetings every day to go over my list of talend jobs. This project lasted a few months until i was laid " + 
-            "off the project because of budget cuts.",
+            "off the project because of budget cuts. I had to return my client laptop back to my manager. Shortly after leaving, i would land a project with client 2.",
             imgUrl: client3
         },
         {
@@ -28,37 +31,68 @@ function Projects()
         },
         {
             title: "Client 3: industry's largest and most experienced benefits providers for health insurance",
-            description: "Design & Development",
+            description: "I was a power bi developer for 3 years working remotely making power bi reports and dashboards. " + 
+            "I made power bi dashboard for each department that contained their power bi reports so they didnt have to keep track of all the links. The reports and dashboards were stored " +
+            "on a premium workspace so the users didnt need a pro license. My day to day routine for the client was attend meetings, make power bi demos, present demo to business users, publish reports using " + 
+            "the prod database, and monitoring power bi reports to make the data is refreshed. They would assign me the user stories that described what the business user wanted to have in their power bi report. " +
+             "After making the demo, they would do user acceptance testing to see if the report looked right. All my reports use sql server as its data source so the only way to test is inputting data into the QA database " + 
+             "and hope to see the expected result in the report. I created power bi alerts using the software tool power automate to let the business users know via email that there are a certain amount in today's report. This gives " +
+             "the business user time to find out what went wrong and solve the issue quickly. They had to let me go because of budget constraints.",
             imgUrl: client2,
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "Security in Computing: Image Steganography",
+            description: "I was in a project group and we decided to make a program that was about steganography. Steganography is the pratice of concealing information within another message " +
+            "or medium to avoid detection. My group decided that we would hide information into another image. We used paint to draw the graphic user interface first before programming so we know " +
+            "what it would look like before creating it in code. We decided to use Wxwidgets to make the graphic user interface which didn't take long to make. After that we divided the work. These are the " +
+            "three objectives: hiding text into an image, hiding an image into another image, and hiding an executable into another image. I picked the 2nd option which was hiding an image into another image. I " + 
+            "used the least significant bit method to hide an image into another image. The least significant bit (LSB) is the bit which is farthest to the right and holds the least value. If someone changed the value " +
+            "to the other's object's binary value then a slight change was made but barely noticeable to the naked eye. I would convert the image to an array of bytes and switch the last 2 bits to the other image's binary numbers. " + 
+            "The picture to your left is an example that i used in my class for image to image. It has a picture inside of it. Im hiding a picture of the joker in the bat symbol. No one will believe you." ,
+            imgUrl: project1,
+            gitUrl: "https://github.com/Inviernos/Cryptology",
+        },
+        {
+            title: "Data Structures: Dijkstra",
+            description: "This c++ program was made to find the shortest path through the mountain. The mountain path was represented by a grayscale map.raw file. 0 means that the mountain was flat and 255 means that the " +
+            "mountain was at its highest. The user would start at the coordinates (0,0) and find the shortest path to (250,200). The hardest step for me was how i was going to get the image data because i had no idea how to import it into " +
+            "my program. I was thinking of using a library at first but after a lot of searching, i finally figured out that i didnt need a library and could just open up the file since a raw file doesn't have any header information like " + 
+            "a png or jpeg has. After getting the image data, i manage to quickly get to work and start using the dijkstra algorithm. After halfway through making the program, i ran into an issue where the program would crash for some reason. " +
+            "it would come and go until i had a light bulb moment of why it was randomly crashing. It was crashing because i was creating a bunch of graph nodes and keeping track of lots of data that i was taking up too much memory. After solving that " +
+            "issue, i finish the program early. I had time to add a feature to ask the user what their new destination was to stop at and it would find the shortest path by coloring the path purple.  ",
+            imgUrl: project2,
+            gitUrl: "https://github.com/Inviernos/Dijkstra",
+        },
+        {
+            title: "Theory of Computation: Matrix-Chain-Multiplication",
+            description: "This c++ program was made to find the minimum cost value of operations it would take to multiply multiple matrices. They don't look at the values of the matrices but use its size to determine the amount of operations. The matrix multiplication is associative so no matter how the product is parenthesized, the result obtained will remain the same. It also prints the matrices in the order that would do the least amount of work. It is a dynamic programming problem that " +
+            "was solved by using memoization. I would compute the minimum cost needed to multiply certain matrix sequences and save it in a table. The program goes through all the calculations to find the minimum cost with every subsequence until you get your value at the " +
+            "top right of the table. During the calculation, you also store the k value in another table to figure out what the order should be for the matrices. I would use recursion in the table with k values to print the order of the matrices. The sequence of matrices would get " +
+            "parenthesize and printed out on the command prompt. An example of the output is shown to the left. This is one of the few dynamic programming programs that i have worked on and enjoyed making. ",
+            imgUrl: project3,
+            gitUrl: "https://github.com/Inviernos/Matrix-Chain-Multiplication",
+        },
+        {
+            title: "Pride Month",
+            description: "HR came to me and a group of people with a task to help make pride flags. HR handed me a bunch of small poles so that i could put the pride flag on there. It took us some time but manage to " + 
+            "complete the task for a good 20 minutes. The next day, HR asked me to take those pride flags and lay them down on the table in a circular fashion on the main floor tables so that people could grab their flags when " +
+            "the time came. HR took pictures of the decorated flags and then ask me to inform people to come down to the main lobby at a certain time for the pride event. We ",
             //imgUrl: projImg1,
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "Christmas Party",
+            description: "HR needed help decorating the new campus for the christmas party. They asked for volunteers and i mentioned that i could help. We moved around furniture, set up snowmen, " + 
+            "santa on a sliegh, trees with light bulbs, etc. We would set up treat stations and games for the children that were coming to this event. I was in charge of the bean bag toss area. There was blue taped lines " + 
+            "that indicated what spot you were going to be at based on your age. Based on your score, the kids would win a prize. The christmas party would start and see a lot of people show up for the event. " +
+            "I joined the christmas party a little later and hanged out with my peers. It was pretty fun. ",
             //imgUrl: projImg1,
         },
         {
-            title: "Business Startup",
-            description: "Design & Development",
-            //imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            //imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
-            //imgUrl: projImg1,
-        },
-        {
-            title: "Business Startup",
-            description: "Design & Development",
+            title: "Halloween",
+            description: "One of the floors at the OneAmerica Tower needed some decoration for halloween. I helped decorate with HR and some others with pumpkins that we made during the carving " + 
+            "pumpkin contest and put it on some tables. Added some fake webs and put paper bats on the ceilings. The floor became a spooky one and  once we were done with the decoration, HR did " + 
+            "their costume contest afterwards. I was with a group of friends who wore lantern shirts. We were the lantern corps and i was the orange one. We each also had our individual color ring. The orange " +
+            "one is the greedy one that says he owns everything.",
             //imgUrl: projImg1,
         },
     ];
@@ -83,7 +117,7 @@ function Projects()
                                     </Nav.Item>
                                     <Nav.Item>
                                         <Nav.Link eventKey="third">
-                                        Helping HR
+                                        HR Events
                                         </Nav.Link>
                                     </Nav.Item>
                                 </Nav>
@@ -122,7 +156,13 @@ function Projects()
                                                         <Accordion.Item eventKey={index.toString()}>
                                                         <Accordion.Header >{project.title}</Accordion.Header>
                                                         <Accordion.Body >
-                                                            {project.description}
+                                                            <div className="d-flex justify-content-between" >
+                                                            <img style={{padding : "5px"}} width="250" height="250" src={project.imgUrl}/> 
+                                                            {project.description} 
+                                                            </div>
+                                                            <div style={{padding : "5px"}}>
+                                                            <a href={project.gitUrl}>Link to Project</a>
+                                                            </div>
                                                         </Accordion.Body>
                                                         </Accordion.Item>
                                                         : ""
