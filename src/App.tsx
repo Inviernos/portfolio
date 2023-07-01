3be3ede134f3467faf64bfd71996de05ed70897f
Anthony Golubski-Allen <anthony302367@gmail.com>
2023-07-01 17:18:32 -0400

    PageChange

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import { Route , Routes} from 'react-router-dom';
-import BI from './pages/PowerBIPage';
+import BIPage from './pages/PowerBIPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/"  element={<Home/>} />
-      <Route path="/PowerBIDemo" element={<BI/>} />
+      <Route path="/PowerBIDemo" element={<BIPage/>} />
     </Routes>
   );
 }
